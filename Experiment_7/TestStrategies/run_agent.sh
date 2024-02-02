186ca5993baf110d49b75cc8bff91841745e2308
bxu0110 <157796276+bxu0110@users.noreply.github.com>
2024-02-02 14:21:03 +0800

    Delete Experiment_7/TestStrategies directory

--- Experiment_7/TestStrategies/run_agent.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --account=def-bolufe
-#SBATCH --cpus-per-task=2         # CPU cores/threads
-#SBATCH --mem-per-cpu=4G      # memory; default unit is megabytes
-#SBATCH --time=120:00:00
-#SBATCH --job-name=TestStrategies
-#SBATCH --output=%x-%j.out
-source ~/TF_RL/bin/activate
-python run_exp.py
