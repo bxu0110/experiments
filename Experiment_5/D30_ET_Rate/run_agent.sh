07e7612db47ae826df2e79c5d7ddac6887384bd3
bxu0110 <157796276+bxu0110@users.noreply.github.com>
2024-02-02 14:16:41 +0800

    Add files via upload

---
+++ Experiment_5/D30_ET_Rate/run_agent.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --account=def-bolufe
+#SBATCH --cpus-per-task=1         # CPU cores/threads
+#SBATCH --mem-per-cpu=2G      # memory; default unit is megabytes
+#SBATCH --time=80:00:00
+#SBATCH --job-name=ET_cnvRate
+#SBATCH --output=%x-%j.out
+source ~/TF_RL/bin/activate
+python run_exp.py
